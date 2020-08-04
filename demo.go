035e0d33cb2eda704904f17d4b56ce2b0b2bda30
zhangjin <zhangjin@cmcm.com>
2020-08-04 13:28:45 +0800

    perf:最简

--- demo.go
+++ demo.go
@@ -1,66 +1,34 @@
-// Package plugindemo a demo plugin.
-package plugindemo
+// Package pluginmkz a demo plugin.
+package pluginmkz
 
 import (
-	"bytes"
 	"context"
-	"fmt"
 	"net/http"
-	"text/template"
 )
 
 // Config the plugin configuration.
 type Config struct {
-	Headers map[string]string `json:"headers,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
-	return &Config{
-		Headers: make(map[string]string),
-	}
+	return &Config{}
 }
 
-// Demo a Demo plugin.
-type Demo struct {
-	next     http.Handler
-	headers  map[string]string
-	name     string
-	template *template.Template
+// Mkz a Mkz plugin.
+type Mkz struct {
+	next http.Handler
+	name string
 }
 
 // New created a new Demo plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	if len(config.Headers) == 0 {
-		return nil, fmt.Errorf("headers cannot be empty")
-	}
-
-	return &Demo{
-		headers:  config.Headers,
-		next:     next,
-		name:     name,
-		template: template.New("demo").Delims("[[", "]]"),
+	return &Mkz{
+		next: next,
+		name: name,
 	}, nil
 }
 
-func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set(key, writer.String())
-	}
-
+func (a *Mkz) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	a.next.ServeHTTP(rw, req)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/containous/plugindemo
+module mikezhang0/traefik-plugin-mkz
 
-go 1.14
+go 1.13
